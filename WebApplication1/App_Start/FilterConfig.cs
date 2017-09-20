b44e2f404b0f6dfe0fb4b5bd4bb2bc25e1d2a100
pofider <jan.blaha@hotmail.com>
2017-09-20 18:24:47 +0200

    add sample

---
+++ WebApplication1/App_Start/FilterConfig.cs
@@ -0,0 +1,21 @@
+﻿using jsreport.Binary;
+using jsreport.Local;
+using jsreport.MVC;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+
+            filters.Add(new JsReportFilterAttribute(new LocalReporting()
+                .UseBinary(JsReportBinary.GetBinary())
+                .AsUtility()
+                .Create()));
+        }
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using jsreport.MVC;
+using jsreport.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [EnableJsReport()]
+        public ActionResult Contact()
+        {
+            HttpContext.JsReportFeature()
+                .Recipe(Recipe.PhantomPdf)
+                .Configure((r) => r.Template.Phantom = new Phantom() { WaitForJS = true });
+            return View();
+        }
+    }
+}
